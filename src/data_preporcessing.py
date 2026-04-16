c64974a71eda3160353a6010d5526db093ad548d
Senitha Gunathilaka <senitha02@gmail.com>
2026-04-16 21:00:19 +0530

    implement data preprocessing functions for inflation prediction

---
+++ src/data_preporcessing.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+
+def select_columns(df):
+    return df[[ 'country_name',
+                'year',
+                'Inflation (CPI %)',
+                'GDP Growth (% Annual)',
+                'Inflation (GDP Deflator, %)',
+                'GDP per Capita (Current USD)']]
+
+#Rename columns
+def rename_columns(df):
+    df.columns = ['Country',
+              'Year',
+              'Inflation_CPI',
+              'GDP_Growth',
+              'Inflation_GDP_Deflator',
+              'GDP_per_Capita']
+
+def clean_data(df):
+
+
+    #drop missing target
+
+    df = df.dropna(subset=['Inflation (CPI %)'])
+
+    #fill missing values for economic indicators
+    cols =['GDP_Growth',
+           'Inflation_GDP_Deflator',
+           'GDP_per_Capita']
+
+    df[cols]= df.groupby('Country')[cols].ffill().bfill()
+
+    #drop remaning missing values
+    df = df.dropna()
+
+    return df
